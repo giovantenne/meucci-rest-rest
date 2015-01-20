632e7313c141eef924a09fc518beea8a01c0ce00
Claudio Benvenuti <claudio.benvenuti@gmail.com>
2015-01-20 10:41:51 +0100

    code refactor

--- app/controllers/studenti_controller.rb
+++ app/controllers/studenti_controller.rb
@@ -2,23 +2,25 @@ class StudentiController < ApplicationController
   before_action :set_studente, only: [:show, :edit, :update, :destroy]
 
   # GET /studenti
+  # GET /studenti.xml
   # GET /studenti.json
   def index
     @studenti = Studente.all
     respond_to do |format|
       format.html # index.html.erb
+      format.xml  { render xml: @studenti }
       format.json { render json: @studenti }
-      format.xml { render xml: @studenti }
     end
   end
 
   # GET /studenti/1
+  # GET /studenti/1.xml
   # GET /studenti/1.json
   def show
     respond_to do |format|
-      format.html # index.html.erb
+      format.html # show.html.erb
+      format.xml  { render xml: @studente }
       format.json { render json: @studente }
-      format.xml { render xml: @studente }
     end
   end
 
@@ -29,60 +31,57 @@ class StudentiController < ApplicationController
 
   # GET /studenti/1/edit
   def edit
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @studente }
-      format.xml { render xml: @studente }
-    end
   end
 
   # POST /studenti
-  # POST /studenti.json
+  # GET /studenti/1.xml
+  # GET /studenti/1.json
   def create
     @studente = Studente.new(studente_params)
 
     respond_to do |format|
       if @studente.save
         format.html { redirect_to @studente, notice: 'Studente was successfully created.' }
-        format.json { render json: @studente }
-        format.xml { render xml: @studente }
+        format.xml { render :show, status: :created, location: @studente }
+        format.json { render :show, status: :created, location: @studente }
       else
         format.html { render :new }
-        format.json { render json: @studente.errors, status: :unprocessable_entity }
         format.xml { render xml: @studente.errors, status: :unprocessable_entity }
+        format.json { render json: @studente.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /studenti/1
+  # PATCH/PUT /studenti/1.xml
   # PATCH/PUT /studenti/1.json
   def update
     respond_to do |format|
       if @studente.update(studente_params)
         format.html { redirect_to @studente, notice: 'Studente was successfully updated.' }
-        format.json { render json: @studente }
-        format.xml { render xml: @studente }
+        format.xml { render :show, status: :ok, location: @studente }
+        format.json { render :show, status: :ok, location: @studente }
       else
         format.html { render :edit }
-        format.json { render json: @studente.errors, status: :unprocessable_entity }
         format.xml { render xml: @studente.errors, status: :unprocessable_entity }
+        format.json { render json: @studente.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /studenti/1
+  # DELETE /studenti/1.xml
   # DELETE /studenti/1.json
   def destroy
     @studente.destroy
     respond_to do |format|
       format.html { redirect_to studenti_url, notice: 'Studente was successfully destroyed.' }
-      format.json { head :no_content }
       format.xml { head :no_content }
+      format.json { head :no_content }
     end
   end
 
   def help
-
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :studenti, :defaults => { :format => 'xml' }
+
+
+  resources :studenti
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
